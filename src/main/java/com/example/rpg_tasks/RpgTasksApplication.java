bf327820cb6a7d51dec6afdc113f88af603599a1
Otavio-Emanoel <otavioemanuel6b@gmail.com>
2025-12-05 23:07:45 -0300

    first commit

---
+++ src/main/java/com/example/rpg_tasks/RpgTasksApplication.java
@@ -0,0 +1,13 @@
+package com.example.rpg_tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rpg_tasks/RpgTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rpg_tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
