1c46d52f1af7015eb2a4f25d3eec6e9c87e7a563
Otavio-Emanoel <otavioemanuel6b@gmail.com>
2025-12-05 23:16:32 -0300

    adiciona a classe Quest com atributos e anotações JPA

---
+++ src/main/java/com/example/rpg_tasks/model/Quest.java
@@ -0,0 +1,22 @@
+package com.example.rpg_tasks.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Quest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private int xpReward;
+    private String difficulty;
+    private boolean completed;
+    
+}
